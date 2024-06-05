9597e13d5fbc6dd3af6ddac8e7b4bb7d89137760
donnelly19 <donamaitsa@gmail.com>
2024-06-05 03:59:05 -0700

    editing binary_trees.h file

--- binary_trees.h
+++ binary_trees.h
@@ -23,6 +23,9 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 /*binary_tree_print.c*/
 void binary_tree_print(const binary_tree_t *);
