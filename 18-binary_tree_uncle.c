a9f0aecdc3d7b94cfd1bcc3f4c68248a71813fd2
donnelly19 <donamaitsa@gmail.com>
2024-06-06 14:10:10 -0700

    finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,13 +6,9 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *grandparent;
-
-	if (node == NULL || node->parent == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-	grandparent = node->parent->parent;
-	if (grandparent->left == node->parent)
-		return (grandparent->right);
-	else
-		return (grandparent->left);
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
