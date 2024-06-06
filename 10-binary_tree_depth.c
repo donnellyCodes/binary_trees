806e378e74eca282ace08b9d2e3bbb890e134de6
donnelly19 <donamaitsa@gmail.com>
2024-06-06 12:31:21 -0700

    measuring depth of a node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,7 +7,7 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 	/*calculate the depth recursively*/
-	return (1 + binary_tree_depth(node->parent));
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
