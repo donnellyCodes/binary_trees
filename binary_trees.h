856df934a77231c49b10611e065a82fd254c3018
donnelly19 <donamaitsa@gmail.com>
2024-06-06 15:24:37 -0700

    editing the header file

--- binary_trees.h
+++ binary_trees.h
@@ -26,7 +26,20 @@ typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-
+/* linked list for advances task 101 levelorder*/
+/**
+ * struct link_s - structure for advances tasks
+ *
+ * @n: depth of node specified
+ * @node: node of the tree to store
+ * @next: next node of the linked list
+ */
+typedef struct link_s
+{
+	size_t n;
+	struct binary_tree_s const *node;
+	struct link_s *next;
+}link_t;
 /*binary_tree_print.c*/
 void binary_tree_print(const binary_tree_t *);
 
