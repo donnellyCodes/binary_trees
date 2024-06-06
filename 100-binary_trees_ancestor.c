a8143a26fb6a757af5be0a61002156a1e70a4b91
donnelly19 <donamaitsa@gmail.com>
2024-06-06 14:45:32 -0700

    finding the lowest common ancestor of two nodes

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: pointer of the first node
+ * @second: pointer of the second node
+ * Return: the ancestor node
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *f, *s;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
+	f = first->parent;
+	s = second->parent;
+	if (s == NULL || first == s || (!f->parent && s))
+		return (binary_trees_ancestor(first, s));
+	else if (s == NULL || f == second || (!s->parent && f))
+		return (binary_trees_ancestor(f, second));
+	return (binary_trees_ancestor(f, s));
+}
--- binary_trees.h
+++ binary_trees.h
@@ -51,6 +51,15 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+/*advanced tasks*/
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_complete(const binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+bst_t *bst_insert(bst_t **tree, int value);
+
 /*helper functions*/
 size_t binary_tree_height_b(const binary_tree_t *tree);
 int height(const binary_tree_t *tree);
