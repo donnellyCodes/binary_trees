0aa70fd002cb9754addd0cdd4bb673ca5f05b471
donnelly19 <donamaitsa@gmail.com>
2024-06-07 03:32:56 -0700

    fixing error on finding perfect binary tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,29 +1,25 @@
 #include "binary_trees.h"
 /**
- * count_nodes - couting nodes of tree
- * @tree: pointer to root node
+ * tree_is_perfect - function that says if a tree is perfect
+ * @tree: tree to check
  * Return: 0
  */
-int count_nodes(const binary_tree_t *tree)
+int tree_is_perfect(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	return (1 + count_nodes(tree->left) + count_nodes(tree->right));
-}
+	int l = 0, r = 0;
 
-/**
- * height - measures height of a binary tree
- * @tree: pointer to the root node
- * Return: height of the tree
- */
-int height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
+	if (tree->left && tree->right)
+	{
+		l = 1 + tree_is_perfect(tree->left);
+		r = 1 + tree_is_perfect(tree->right);
+		if (r == l && r != 0 && l != 0)
+			return (r);
 		return (0);
-	int left_height = height(tree->left);
-	int right_height = height(tree->right);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
+	}
+	else if (!tree->left && !tree->right)
+		return (1);
+	else
+		return (0);
 }
 
 /**
@@ -33,18 +29,12 @@ int height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int h;
-
-	int expected_nodes;
-
-	int actual_nodes;
+	int result = 0;
 
 	if (tree == NULL)
 		return (0);
-	/*calculate height of tree*/
-	h = height(tree);
-	/*check if number of nodes matches perfect binary tree formula*/
-	expected_nodes = (1 << h) - 1;
-	actual_nodes = count_nodes(tree);
-	return (actual_nodes == expected_nodes);
+	result = tree_is_perfect(tree);
+	if (result != 0)
+		return (1);
+	return (0);
 }
